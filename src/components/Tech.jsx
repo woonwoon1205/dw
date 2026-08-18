2def7c75e8ec14e248dff2f859004b431c17282d
woonwoon1205 <saladin707@gmail.com>
2026-08-18 13:44:39 +0900

    Update technology display logic in Tech component

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -14,8 +14,11 @@ const Tech = () => {
       </motion.div>
 
       <div className="flex flex-wrap justify-center gap-10 mt-14">
-        {technologies.map((technology) => (
-          <div className="w-28 h-28" key={technology.name}>
+                {technologies.map((technology, index) => (
+          <div
+            className={`w-28 h-28 ${index >= 8 ? 'max-md:hidden' : ''}`}
+            key={technology.name}
+          >
             <BallCanvas icon={technology.icon} />
           </div>
         ))}
