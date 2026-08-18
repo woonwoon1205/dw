631dd983c8e4b503e1e270691467c477b5285c3f
woonwoon1205 <saladin707@gmail.com>
2026-08-18 14:04:06 +0900

    Update React plugin import in Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-refresh'
-
+import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/dw/',
